42d994187aca386fa09fd86fc40dadec5001f3f2
Caleb <kalebsam@hotmail.com>
2024-07-15 10:44:53 -0500

    fixes

--- admin-prueba/src/environments/environment.ts
+++ admin-prueba/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     name: 'dev',
-    url_api: 'http://54.221.164.208:8001/'
+    url_api: 'https://2d1b-191-107-4-238.ngrok-free.app/'
 };
 
 /*
